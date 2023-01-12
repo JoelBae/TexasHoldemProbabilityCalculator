5e5f95132156e0120a851ff2fa6558ad27c618ee
JoelBae <79932976+JoelBae@users.noreply.github.com>
2023-01-11 19:31:56 -0500

    Delete TexasHoldem directory

--- TexasHoldem/BestScoreHand.cpp
+++
@@ -1,3 +0,0 @@
-#include "BestScoreHand.h"
-
-BestScoreHand::BestScoreHand(std::vector<Card> hand, int score) : hand{hand}, score{score} {}
--- TexasHoldem/Card.cpp
+++
@@ -1,5 +0,0 @@
-#include "Card.h"
-Card::Card(int suit, int rank) : suit(suit), rank(rank) {}
-bool Card::operator==(const Card& other) const {
-	return (suit == other.suit && rank == other.rank);
-}
